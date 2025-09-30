6449c4f909f9437b21361eff6de6363bc8f5a206
Stivent Nathaniel Riwu Laky <stivitsme@gmail.com>
2025-09-30 14:02:22 +0700

    update dari stivent

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,8 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title ?? 'Exordia' }}</title>
+    <title>{{ $title ?? 'PT. Kupang Tour & Travel' }}</title>
 
+    <link rel="icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
     {{-- Bootstrap CSS via CDN --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,5 +1,4 @@
-{{-- resources/views/auth/login.blade.php --}}
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -42,4 +41,43 @@
         </div>
     </div>
 </body>
-</html>
+</html> --}}
+{{-- resources/views/auth/login.blade.php --}}
+<x-layouts.app title="Login">
+    <div class="login-container">
+        <div class="left-panel">
+            <img src="{{ asset('images/logoKTTputih.png') }}" alt="Logo KTT" class="logo">
+            <h4>Halo, selamat datang</h4>
+            <h1>LOGIN <br> DATABASE</h1>
+        </div>
+
+        <div class="right-panel">
+            <div class="login-box">
+                <h2>LOGIN</h2>
+                <p class="subtitle">DATABASE PT. KUPANG TOUR & TRAVEL</p>
+
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class="input-group">
+                        <img src="{{ asset('images/userLogin.png') }}" class="icon" alt="user">
+                        <input type="text" name="username" placeholder="Username" required>
+                    </div>
+                    <div class="input-group">
+                        <img src="{{ asset('images/userPass.png') }}" class="icon" alt="password">
+                        <input type="password" name="password" placeholder="Password" required>
+                    </div>
+
+                    <button type="submit" class="btn-login">Login Now</button>
+                </form>
+
+                <div class="extra">
+                    <span>Forget Password?</span>
+                    <a href="#">Change Password</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- Tambahkan CSS khusus login --}}
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+</x-layouts.app>
