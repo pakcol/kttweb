38da091c393cf8d4add439a9bf82ef9eb6b27fad
Stivent Nathaniel Riwu Laky <stivitsme@gmail.com>
2025-10-05 18:33:37 +0700

    update tampilan home database

--- resources/views/homeDb.blade.php
+++ resources/views/homeDb.blade.php
@@ -1,22 +1,36 @@
-<x-layouts.app title="HomeDb">
-    <section class="full-bleed hero">
-        <div class="container">
-        <div class="left-panel">
-            <h4>Halo, selamat datang <span>{{ Auth::user()->name ?? 'User' }}</span>!</h4>
-            <h1>DATABASE</h1>
-            <p>PT. Kupang Tour & Travel</p>
-            <button class="btn-input" onclick="window.location.href='{{ url('/input-data') }}'">Input Data</button>
-        </div>
+<x-layouts.app title="Home Database">
 
-        <div class="right-panel">
-            <img src="{{ asset('images/ikon_6.png') }}" alt="Database Illustration">
+    <!-- Main Content -->
+    <section class="home-database-section">
+        <div class="container-fluid h-100">
+            <div class="row align-items-center h-100">
+                <!-- Left Panel - Content -->
+                <div class="col-lg-6 left-panel">
+                    <div class="content-wrapper">
+                        <h4 class="welcome-text">Halo, selamat datang <span class="user-name">{{ Auth::user()->name ?? 'User' }}</span>!</h4>
+                        <h1 class="main-title">DATABASE</h1>
+                        <p class="company-name">PT. Kupang Tour & Travel</p>
+                        <button class="btn-input" onclick="window.location.href='{{ url('/input-data') }}'">
+                            Input Data
+                        </button>
+                    </div>
+                </div>
+
+                <!-- Right Panel - Illustration -->
+                <div class="col-lg-6 right-panel">
+                    <div class="image-wrapper">
+                        <img src="{{ asset('images/ikon_6.png') }}" alt="Database Illustration" class="img-fluid">
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
+    </section>
 
     <!-- Logout form -->
     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
         @csrf
     </form>
-    </section>
-</x-layouts.app>
 
+    <!-- Link CSS External -->
+    <link rel="stylesheet" href="{{ asset('css/homeDatabase.css') }}">
+</x-layouts.app>
